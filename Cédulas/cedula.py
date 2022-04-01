93a36939a19401e25d837e6c413ad3d3cb21c00d
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-01 14:29:37 -0300

    pegando o resto da divisão de um numero por 10

--- Cédulas/cedula.py
+++ Cédulas/cedula.py
@@ -9,6 +9,9 @@ nota20 = notaDe20 // 20
 notaDe10 = notaDe20 % 20
 
 nota10 = notaDe10 // 10
+notaDe05 = notaDe10 % 10
+
 print('{} nota(s) de R$ 100,00 '.format(nota100))
 print('{} nota(s) de R$ 50,00 '.format(nota50))
 print('{} nota(s) de R$ 20,00 '.format(nota20))
+print('{} nota(s) de R$ 20,00 '.format(nota10))
