16340eab02b4cd44c9938b3290d92b5a32997e36
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-01 14:24:52 -0300

    pegando a divisão inteira de um numero e dividindo por 20

--- Cédulas/cedula.py
+++ Cédulas/cedula.py
@@ -4,5 +4,8 @@ notaDe50 = nota % 100
 
 nota50 = notaDe50 // 50
 notaDe20 = notaDe50 % 50
+
+nota20 = notaDe20 // 20
+
 print('{} nota(s) de R$ 100,00 '.format(nota100))
 print('{} nota(s) de R$ 50,00 '.format(nota50))
