01adaf10ccb2d297c1494eff88716d325eafcb1a
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-04-01 14:28:24 -0300

    pegando uma divisao inteira de um numero epor 10

--- Cédulas/cedula.py
+++ Cédulas/cedula.py
@@ -7,6 +7,8 @@ notaDe20 = notaDe50 % 50
 
 nota20 = notaDe20 // 20
 notaDe10 = notaDe20 % 20
+
+nota10 = notaDe10 // 10
 print('{} nota(s) de R$ 100,00 '.format(nota100))
 print('{} nota(s) de R$ 50,00 '.format(nota50))
 print('{} nota(s) de R$ 20,00 '.format(nota20))
